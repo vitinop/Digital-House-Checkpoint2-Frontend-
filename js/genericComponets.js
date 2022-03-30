1aea8520f8bef337facba92aea98dfc48c8fc165
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-03-30 15:58:23 -0300

    update

---
+++ js/genericComponets.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('#defaultHeader');
+nav.innerHTML = 
+    `<nav>
+    
+  </nav>`;
+
+const foot = document.querySelector('#defaultFooter');
+foot.innerHTML = 
+`
+<footer>
+ 
+</footer>
+`;
