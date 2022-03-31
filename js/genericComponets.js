ae590c13ec07c1a1777362f2b89b01312544172b
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-03-30 23:59:45 -0300

    Update genericComponets.js

--- js/genericComponets.js
+++ js/genericComponets.js
@@ -7,7 +7,88 @@ nav.innerHTML =
 const foot = document.querySelector('#defaultFooter');
 foot.innerHTML = 
 `
-<footer>
- 
+<footer class="container-fluid">
+
+    <section class="teamSection">
+
+        <div class="aluno">
+            <p>Luanderson P.</p>
+
+            <a target="_blank" href="https://github.com/LuandersonPontes/CTD" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/luanderson-pontes-790348231/" class="social">
+                <i class="bi bi-linkedin"></i></a>
+        </div>
+
+        <div class="aluno">
+            <p>Matheus E.</p>
+
+            <a target="_blank" href="https://github.com/fehbr800" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="#" class="social">
+                <i class="bi bi-linkedin"></i>
+            </a>
+        </div>
+
+        <div class="aluno">
+            <p>Adriano A.</p>
+            <a target="_blank" href="https://github.com/Laguiosta" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="#" class="social">
+                <i class="bi bi-linkedin"></i></a>
+        </div>
+
+        <div class="aluno">
+            <p>Victor L.</p>
+            <a target="_blank" target="_blank" href="https://github.com/vitinop" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/victor-luz-7a531068" class="social">
+                <i class="bi bi-linkedin"></i></a>
+        </div>
+
+        <div class="aluno">
+            <p>Jefferson Mendes</p>
+            <a target="_blank" href="https://github.com/JeffersonMendes32" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="#" class="social">
+                <i class="bi bi-linkedin"></i>
+            </a>
+        </div>
+
+        <div class="aluno">
+            <p>Natali Souza</p>
+            <a target="_blank" href="https://github.com/naay12" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/nataly-souza-a9237622b" class="social">
+                <i class="bi bi-linkedin"></i>
+            </a>
+        </div>
+
+        <div class="aluno">
+            <p>Renan A.</p>
+            <a target="_blank" href="https://github.com/devrsantos" class="social">
+                <i class="bi bi-github"></i>
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/in/renan-augusto-santos-568422222" class="social">
+                <i class="bi bi-linkedin"></i>
+            </a>
+        </div><!--aluno-->
+
+    </section><!--teamsection-->
+
+
+
+    <section class="copyrightSection">
+        <p>Checkpoint desenvolvido em 2022</p>
+
+    </section><!--copyright-->
+
+
 </footer>
 `;
