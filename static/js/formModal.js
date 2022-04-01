504657e29818eac9c4c905a54b176aefb13046ea
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-04-01 01:21:18 -0300

    Construção do Modal

--- static/js/formModal.js
+++ static/js/formModal.js
@@ -1,25 +1,19 @@
-// Get do Modal
-var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
-var btn = document.getElementById("btn-01");
+let iconPlay = document.querySelector('.iconPlay');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let modal = document.querySelector('.modal');
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+let iconClose = document.querySelector('.iconClose');
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// When the user clicks the button, open the modal
+iconPlay.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+iconClose.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+    if (e.target == modal) modal.style.display = 'none';
+});
