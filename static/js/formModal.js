8a2a626a5943423fca7eefe5b58b162c03308c01
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-04-01 03:41:14 -0300

    Atualização de folhas de estilo, e Estrutura do HTML

--- static/js/formModal.js
+++ static/js/formModal.js
@@ -1,19 +1,25 @@
+// Get do Modal
+var modal = document.getElementById("myModal");
+
 // Get the button that opens the modal
-let iconPlay = document.querySelector('.iconPlay');
+var btn = document.getElementById("btn-01");
 
-let modal = document.querySelector('.modal');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-let iconClose = document.querySelector('.iconClose');
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-// When the user clicks the button, open the modal
-iconPlay.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-iconClose.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-window.addEventListener('click', (e) => {
-    if (e.target == modal) modal.style.display = 'none';
-});
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
